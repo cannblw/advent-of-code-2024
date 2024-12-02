a5ae08330b25eac59ad516e2aee1e2ea6f38d4e9
Edgar Chirivella <edgar.95.12@gmail.com>
2024-12-02 20:00:54 +0100

    02-red-nosed-reports part 2

---
+++ 02-red-nosed-reports/part-2/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const dampenerTolerance = 1
+
+func main() {
+	startTime := time.Now()
+	file, err := os.Open("./input")
+	if err != nil {
+		log.Fatalf("Error reading input: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	safeCount := 0
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+
+		tokens := strings.Fields(line)
+
+		secondItem, err := strconv.Atoi(tokens[1])
+		if err != nil {
+			log.Fatalf("Error converting item %s to int: %v", tokens[1], err)
+		}
+
+		firstItem, err := strconv.Atoi(tokens[0])
+		if err != nil {
+			log.Fatalf("Error converting item %s to int: %v", tokens[0], err)
+		}
+
+		increasing := secondItem > firstItem
+
+		currentDampenerStatus := 0
+		i := 1
+
+		for ; i < len(tokens); i++ {
+			secondItem, err := strconv.Atoi(tokens[i])
+			if err != nil {
+				log.Fatalf("Error converting item %s to int: %v", tokens[i], err)
+			}
+			firstItem, err := strconv.Atoi(tokens[i-1])
+			if err != nil {
+				log.Fatalf("Error converting item %s to int: %v", tokens[i-1], err)
+			}
+
+			if secondItem == firstItem {
+				if currentDampenerStatus == dampenerTolerance {
+					break
+				}
+
+				currentDampenerStatus++
+			}
+
+			if secondItem > firstItem && (!increasing || secondItem-firstItem > 3) {
+				if currentDampenerStatus == dampenerTolerance {
+					break
+				}
+
+				currentDampenerStatus++
+			}
+
+			if secondItem < firstItem && (increasing || firstItem-secondItem > 3) {
+				if currentDampenerStatus == dampenerTolerance {
+					break
+				}
+
+				currentDampenerStatus++
+			}
+		}
+
+		if i == len(tokens) {
+			safeCount++
+		}
+	}
+
+	log.Printf("The result is %d\n. Took: %d microseconds", safeCount, time.Since(startTime).Microseconds())
+}
